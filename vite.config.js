6d9dadf56ee68bf9f70d653094aacdb1e5c9c13e
Krishna Shukla <backend.9developer@gmail.com>
2025-06-28 02:13:46 +0530

    fix: correct vite config and enable SPA routing

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build', // So Render can find it
-  },
-  base: './', // Relative asset paths
+  plugins: [
+    tailwindcss(),
+  ],
+  
 })
